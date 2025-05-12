a2a85eb367315fd24b53151eecb23cc9d79a3c27
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2025-05-12 21:45:02 +0900

    add dangerous code test

---
+++ demo/test/test_dangerous.gd
@@ -0,0 +1,44 @@
+extends GutTest
+
+var res : ReDScribe = null
+var result
+
+func before_each():
+	res = ReDScribe.new()
+	res.method_missing.connect(method_missing)
+	result = null
+
+
+func method_missing(_method_name, args):
+	if args: result = args[0]
+
+
+func test_exit_fails():
+	res.perform('exit')
+	assert_eq(res.exception, '')
+	res.perform('foo 1')
+	assert_eq(result, 1)
+
+
+func test_send_exit_fails():
+	res.perform('send :exit')
+	assert_eq(res.exception, '')
+	res.perform('foo 1')
+	assert_eq(result, 1)
+
+
+func test_shell_command_fails():
+	res.perform('foo `whoami`')
+	assert_eq(result, null)
+	res.perform('foo 1')
+	assert_eq(result, 1)
+
+
+func test_file_read_works():
+	res.perform("foo File.read('project.godot')")
+	assert_not_null(result)
+
+
+func test_dir_pwd_works():
+	res.perform('foo Dir.pwd')
+	assert_not_null(result)
--- demo/test/test_exception.gd
+++ demo/test/test_exception.gd
@@ -1,34 +1,34 @@
 extends GutTest
 
+var res : ReDScribe = null
+
+func before_each():
+	res = ReDScribe.new()
+
 
 func test_method_missing():
-	var res = ReDScribe.new()
 	res.perform('foo')
 	assert_eq(res.exception, '')
 
 
 func test_simple_calc():
-	var res = ReDScribe.new()
 	res.perform('1 + 1')
 	assert_eq(res.exception, '')
 
 
 func test_syntax_error():
-	var res = ReDScribe.new()
 	res.perform('1 +')
 	assert_eq(res.exception, 'syntax error (SyntaxError)')
 
 
 func test_raise():
-	var res = ReDScribe.new()
 	res.perform('raise "テスト"')
 	assert_eq(res.exception, 'テスト (RuntimeError)')
 
 
 func test_multiple_instances():
-	var res1 = ReDScribe.new()
 	var res2 = ReDScribe.new()
-	res1.perform('foo')
+	res.perform('foo')
 	res2.perform('raise')
-	assert_eq(res1.exception, '')
+	assert_eq(res.exception, '')
 	assert_eq(res2.exception, 'RuntimeError')
