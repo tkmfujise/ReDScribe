94d5fd1141038e62e9ec6f5b234976a5e2381c49
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2025-05-26 23:10:31 +0900

    [mrblib/resource] update

--- demo/addons/redscribe/mrblib/resource.rb
+++ demo/addons/redscribe/mrblib/resource.rb
@@ -1,9 +1,10 @@
 class Resource
-  attr_accessor :_key, :_children, :name 
+  attr_accessor :_key, :_children, :_mapping, :name 
 
   def initialize(key, name = nil, &block)
     self._key      = key
-    self.name      = name || key.to_s
+    self.name      = name || "#{key}_#{object_id}"
+    self._mapping  = {}
     self._children = []
     instance_exec(&block) if block_given?
 	end
@@ -19,7 +20,13 @@ class Resource
   def attributes
     hash = attribute_keys.map{|k| [k.to_sym, self[k]] }.to_h
     _children.group_by(&:_key).map do |k, arr|
-      hash.merge!(k => arr.map(&:attributes))
+      if _mapping[k]
+        if _mapping[k] == k
+          hash.merge!(k => arr[0].attributes)
+        else
+          hash.merge!(_mapping[k] => arr.map(&:attributes))
+        end
+      end
     end
     hash
   end
@@ -30,7 +37,9 @@ class Resource
 
   def method_missing(method_name, *args, &block)
     if block_given?
-      child = Resource.new(method_name)
+      super unless _mapping[method_name]
+      name  = args[0]
+      child = Resource.new(method_name, name)
       child.instance_exec(&block)
       _children << child
     elsif args.count == 1
@@ -41,16 +50,49 @@ class Resource
   end
 end
 
+$main = self
 
-# TODO
+class DSL
+  attr_accessor :mapping
+  
+  def initialize
+    self.mapping = {}
+  end
+
+  def resource(key, top_level: false, &block)
+    instance_exec(&block) if block_given?
+    recv = top_level ? $main : self
+    defined_mapping = mapping
+    defined_mapping.merge!(key => key) unless top_level
+    recv.define_singleton_method(key) do |name = nil, &blk|
+      res = Resource.new(key, name)
+      res._mapping.merge!(defined_mapping)
+      res.instance_exec(&blk)
+      res.tap(&:emit) if top_level
+    end
+  end
+  
+  def resources(mapping, &block)
+    key, _ = mapping.to_a[0]
+    self.mapping.merge!(mapping)
+    instance_exec(&block) if block_given?
+    define_singleton_method(key) do |name = nil, &blk|
+      res = Resource.new(key, name)
+      res.instance_exec(&blk)
+    end
+  end
+end
+
+
 # resource :chapter do
 #   resource :image
 #   resources :stage => :stages
 # end
 def resource(key, &block)
-  define_method(key) do |name = nil, &blk|
-    res = Resource.new(key, name)
-    res.instance_exec(&blk)
-    res.tap(&:emit)
+  dsl = DSL.new
+  if block_given?
+    dsl.resource(key, top_level: true){ instance_exec(&block) }
+  else
+    dsl.resource(key, top_level: true)
   end
 end
--- demo/test/mrblib/test_resource.gd
+++ demo/test/mrblib/test_resource.gd
@@ -10,8 +10,8 @@ func before_each():
 	result = null
 
 
-func _subscribe(resource_name, attributes):
-	result = { 'name': resource_name, 'attributes': attributes }
+func _subscribe(key, attributes):
+	result = { 'key': key, 'attributes': attributes }
 
 
 func test_simple():
@@ -19,12 +19,162 @@ func test_simple():
 		resource :player
 		player 'Alice' do
 		  level 1
-		  job   :magician
+		  job   :wizard
 		end
 	""")
-	assert_eq(result['name'], 'player')
+	assert_eq(result['key'], 'player')
 	assert_eq(result['attributes'], {
 		&'name':  'Alice',
 		&'level': 1,
-		&'job':   &'magician',
+		&'job':   &'wizard',
+	})
+
+
+func test_without_args():
+	res.perform("""
+		resource :player
+		player {}
+	""")
+	assert_eq(result['key'], 'player')
+	var regex = RegEx.new()
+	regex.compile('^player_\\d+$')
+	assert_not_null(regex.search(result['attributes']['name']))
+
+
+func test_undefined():
+	res.perform("""
+		player {}
+	""")
+	assert_eq(result, null)
+
+
+func test_with_block():
+	res.perform("""
+		resource :player do
+		  resource :image
+		end
+		player 'Alice' do
+		  level 1
+		  image 'avater' do
+			path 'path/to/image.png'
+		  end
+		end
+	""")
+	assert_eq(result['key'], 'player')
+	assert_eq(result['attributes'], {
+		&'name':  'Alice',
+		&'level': 1,
+		&'image': {
+			&'name': 'avater',
+			&'path': 'path/to/image.png',
+		},
+	})
+
+
+func test_with_block_no_args():
+	res.perform("""
+		resource :player do
+		  resource :image
+		end
+		player do
+		  level 1
+		  image do
+			path 'path/to/image.png'
+		  end
+		end
+	""")
+	assert_eq(result['key'], 'player')
+	var regex = RegEx.new()
+	regex.compile('^player_\\d+$')
+	assert_not_null(regex.search(result['attributes']['name']))
+	assert_eq(result['attributes']['level'], 1)
+	regex.compile('^image_\\d+$')
+	assert_not_null(regex.search(result['attributes']['image']['name']))
+	assert_eq(result['attributes']['image']['path'], 'path/to/image.png')
+
+
+func test_with_block_array():
+	res.perform("""
+		resource :player do
+		  resources :image => :images
+		end
+		player 'Alice' do
+		  level 1
+		  image 'avater' do
+			path 'path/to/avater.png'
+		  end
+		  image 'background' do
+			path 'path/to/background.png'
+		  end
+		end
+	""")
+	assert_eq(result['key'], 'player')
+	assert_eq(result['attributes'], {
+		&'name':  'Alice',
+		&'level': 1,
+		&'images': [
+			{
+				&'name': 'avater',
+				&'path': 'path/to/avater.png',
+			},
+			{
+				&'name': 'background',
+				&'path': 'path/to/background.png',
+			},
+		],
+	})
+
+
+func test_with_block_undefined():
+	res.perform("""
+		resource :player
+		player 'Alice' do
+		  image do
+			'test'
+		  end
+		end
+	""")
+	assert_eq(result['key'], 'player')
+	assert_eq(result['attributes'], {
+		&'name':  'Alice',
+	})
+
+
+func test_with_full():
+	res.perform("""
+		resource :chapter do
+		  resource :image
+		  resources :stage => :stages
+		end
+		chapter 'Opening' do
+		  level 1
+		  image 'thumbnail' do
+			path 'path/to/opening.png'
+		  end
+		  stage 'stage1' do
+			level 1
+		  end
+		  stage 'stage2' do
+			level 2
+		  end
+		end
+	""")
+	assert_eq(result['key'], 'chapter')
+	assert_eq(result['attributes'], {
+		&'name':  'Opening',
+		&'level': 1,
+		&'image': {
+			&'name': 'thumbnail',
+			&'path': 'path/to/opening.png',
+		},
+		&'stages': [
+			{
+				&'name':  'stage1',
+				&'level': 1,
+			},
+			{
+				&'name':  'stage2',
+				&'level': 2,
+			},
+		],
 	})
