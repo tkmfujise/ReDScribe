81b9be111f98716e65f675afbaddbc8adf7a8354
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2025-05-29 23:40:39 +0900

    [mrblic/core_ext/string] String#chars override for UTF-8

---
+++ demo/test/mrblib/core_ext/test_string.gd
@@ -0,0 +1,35 @@
+extends GutTest
+
+var res : ReDScribe = null
+const EXT_PATH = 'addons/redscribe/mrblib/core_ext/string'
+const FETCH_SIGNAL = 'fetch_value'
+var fetched_value
+
+
+func before_each():
+	res = ReDScribe.new()
+	res.channel.connect(_subscribe)
+	fetched_value = null
+
+
+func _subscribe(key, payload):
+	match key:
+		FETCH_SIGNAL: fetched_value = payload
+		_: return
+
+
+func require_ext():
+	res.perform('require "%s"' % EXT_PATH)
+
+
+func fetch(code) -> Variant:
+	res.perform("Godot.emit_signal :%s, (%s)" % [FETCH_SIGNAL, code])
+	return fetched_value
+
+
+
+func test_chars():
+	var arr = ['a', 'b', 'c', 'あ', 'い', 'う', '🍣', '🍺']
+	assert_ne(fetch("'abcあいう🍣🍺'.chars"), arr)
+	require_ext()
+	assert_eq(fetch("'abcあいう🍣🍺'.chars"), arr)
